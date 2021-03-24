59eb5bb76bced1df7f21dae222140fe61748c008
gityBoiii <gnewid88@gmail.com>
2021-03-24 11:02:05 +0900

    ...
    ...

---
+++ 4.function.py
@@ -0,0 +1,17 @@
+# def sum(a, b):
+#     return a+b
+#
+# a=3;b=4
+# c=sum(a,b)
+# print("a + b = c :",c)
+#
+# def say():
+#     return "hi"
+#
+# print(say())
+#
+# def sum(a,b):
+#     d = print("%d + %d = %d"% (a,b,a+b))
+#
+# sum(3,6)
+# print("d : ", d) ##결과값 반환 x
